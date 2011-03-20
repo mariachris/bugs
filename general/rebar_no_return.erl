f2051778e4e21f4ded3a4dffa953b375ca767986
Maria Christakis <mchrista@softlab.ntua.gr>
2011-03-20 19:29:57 +0200

    Minimize test

--- general/rebar_no_return.erl
+++ general/rebar_no_return.erl
@@ -2,8 +2,6 @@
 
 -export([t/0]).
 
--define(ABORT(Str, Args), abort(Str, Args)).
-
 -spec t() -> no_return().
 t() ->
   F = log_and_halt("baz"),
@@ -12,9 +10,9 @@ t() ->
 -spec log_and_halt(string()) -> fun((string(),integer()) -> no_return()).
 log_and_halt(Msg) ->
   fun(_, _) ->
-      ?ABORT(Msg, [])
+      abort(Msg)
   end.
 
-abort(Str, _Args) ->
-  io:format("~s~n", [Str]),
+abort(Msg) ->
+  io:format("~s~n", [Msg]),
   halt(1).
